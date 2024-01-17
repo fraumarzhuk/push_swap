771e71a88def16f35b4fe5ead98351fe4e4880fb
mzhukova <mzhukova@student.42Wolfsburg.de>
2024-01-17 19:07:08 +0100

    compiles with multiple arguments

--- main.c
+++ main.c
@@ -24,18 +24,28 @@ int main(int argc, char **argv)
     if (argc < 2 || (argc > 2 && !argv[1][0]))
     // TODO:
     //Handle error handling in all the cases(incorrect input zbs)
-    // Create 2 stacks;
+    //Error with dublicated inputs
+    //Create 2 stacks;
         return (ft_printf("Please compile with arguments. Programm failed. Just like your parents did.\n"));
     //Cover the cases with one ane argument in a string, or arguments provided without strings
     Stack *stack_a = malloc(sizeof(Stack));
     if (stack_a == NULL)
         exit(2);
-    stack_a->value = ft_atoi(argv[1]);
+    // stack_a->value = ft_atoi(argv[1]);
     stack_a->next = NULL;
-    int i = 2;
+    int i = 1;
+    char **input;
     while (i < argc) //skipping the name of the file
     {
-        insert_end(&stack_a, ft_atoi(argv[i]));
+       input = ft_split(argv[i], ' ');
+        while (*input)
+        {
+            if(!stack_a->value)
+                stack_a->value = ft_atoi(*input);
+            else
+                insert_end(&stack_a, ft_atoi(*input));
+            input++;         
+        }
         i++;
     }
     //print the values for testing purposes:
