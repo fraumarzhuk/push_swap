6f1900ef75b13e0ba6d89411bd161cb96889ccee
mzhukova <mzhukova@student.42Wolfsburg.de>
2024-01-29 17:25:10 +0100

    helper functions added

--- utils2.c
+++ utils2.c
@@ -6,7 +6,7 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 14:19:02 by mzhukova          #+#    #+#             */
-/*   Updated: 2024/01/29 17:01:58 by mzhukova         ###   ########.fr       */
+/*   Updated: 2024/01/29 17:23:10 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,28 +34,27 @@ void rb(Stack **stack_b)
 // rr : ra and rb at the same time.
 void rr(Stack *stack_a, Stack *stack_b)
 {
-    ra(stack_a);
-    rb(stack_b);
+    ra(&stack_a);
+    rb(&stack_b);
 }
 // rra (reverse rotate a): Shift down all elements of stack a by 1.
 // The last element becomes the first one.
 void rra(Stack **stack_a)
 {
-    Stack *last, *second_to_last;
-
+    Stack *last;
+    Stack *second_to_last;
+    
     if (*stack_a == NULL || (*stack_a)->next == NULL)
         return; // Do nothing if the stack is empty or has only one element
 
     last = *stack_a;
     second_to_last = NULL;
-
     // Find the last and second-to-last elements
     while (last->next != NULL)
     {
         second_to_last = last;
         last = last->next;
     }
-
     // Perform the reverse rotation
     if (second_to_last != NULL)
     {
@@ -63,18 +62,36 @@ void rra(Stack **stack_a)
         last->next = *stack_a;
         *stack_a = last;
     }
-
     ft_printf("rra\n");
 }
 // rrb (reverse rotate b): Shift down all elements of stack b by 1.
 // The last element becomes the first one.
-void rrb(Stack *stack_a, Stack *stack_b)
+void rrb(Stack **stack_b)
 {
+    Stack *last;
+    Stack *second_to_last;
+    
+    if (*stack_b == NULL || (*stack_b)->next == NULL)
+        return;
+    last = *stack_b;
+    second_to_last = NULL;
+    
+    while(last->next != NULL)
+    {
+        second_to_last = last;
+        last = last->next;
+    }
+    if (second_to_last != NULL)
+    {
+        second_to_last->next = NULL;
+        last->next = *stack_b;
+        *stack_b = last;
+    }
     ft_printf("rrb\n");
 }
 // rrr : rra and rrb at the same time.
 void rrr(Stack *stack_a, Stack *stack_b)
 {
-    rra(stack_a, stack_b);
-    rrb(stack_a, stack_b);
+    rra(&stack_a);
+    rrb(&stack_b);
 }
