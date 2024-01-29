3d15a6bc2282fd6ebb2fb513ce99d72e7ce43e3c
mzhukova <mzhukova@student.42Wolfsburg.de>
2024-01-29 17:03:15 +0100

    changes

--- utils2.c
+++ utils2.c
@@ -6,7 +6,7 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 14:19:02 by mzhukova          #+#    #+#             */
-/*   Updated: 2024/01/29 14:23:53 by mzhukova         ###   ########.fr       */
+/*   Updated: 2024/01/29 17:01:58 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,56 @@
 
 // rb (rotate b): Shift up all elements of stack b by 1.
 // The first element becomes the last one.
-void rb(Stack *stack_a, Stack *stack_b)
+void rb(Stack **stack_b)
 {
+    Stack *first;
+    Stack *last;
+
+    if (*stack_b == NULL || (*stack_b)->next == NULL)
+        return;
+    first = *stack_b;
+    last = *stack_b;
+    
+    while(last->next != NULL)
+        last = last->next;
+    *stack_b = first->next;
+    first->next = NULL;
+    last->next = first;
     ft_printf("rb\n");
 }
 // rr : ra and rb at the same time.
 void rr(Stack *stack_a, Stack *stack_b)
 {
-    ra(stack_a, stack_b);
-    rb(stack_a, stack_b);
+    ra(stack_a);
+    rb(stack_b);
 }
 // rra (reverse rotate a): Shift down all elements of stack a by 1.
 // The last element becomes the first one.
-void rra(Stack *stack_a, Stack *stack_b)
+void rra(Stack **stack_a)
 {
+    Stack *last, *second_to_last;
+
+    if (*stack_a == NULL || (*stack_a)->next == NULL)
+        return; // Do nothing if the stack is empty or has only one element
+
+    last = *stack_a;
+    second_to_last = NULL;
+
+    // Find the last and second-to-last elements
+    while (last->next != NULL)
+    {
+        second_to_last = last;
+        last = last->next;
+    }
+
+    // Perform the reverse rotation
+    if (second_to_last != NULL)
+    {
+        second_to_last->next = NULL;
+        last->next = *stack_a;
+        *stack_a = last;
+    }
+
     ft_printf("rra\n");
 }
 // rrb (reverse rotate b): Shift down all elements of stack b by 1.
